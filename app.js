dcc3e3210297c77bf62068e61f1a05e7d17c4748
Travers McInerney <travers@mcinerney.me>
2013-06-28 12:54:26 +1000

    Tags are now loading

--- app.js
+++ app.js
@@ -8,8 +8,8 @@
       'click .contact h5'               : 'toggleContactSection',
       'click .groups h5'                : 'getGroupsForContact',
       'click .orders-subscriptions h5'  : 'getOrdersAndSubscriptionsForContact',
-      'click .goto-search'              : 'gotoSearch',
       'click .search-button'            : 'onSearch',
+      'click .toggle-search'            : 'toggleSearch',
       'ticket.requester.email.changed'  : 'onSearch'
     },
     requests: {
@@ -34,13 +34,101 @@
       var app         = this,
           serviceData = {
             privateKey: app.settings.token
+          },
+
+          // private
+          _createRequest = function(xml) {
+            return {
+              contentType : 'application/xml',
+              data        : xml,
+              dataType    : 'xml',
+              proxy_v2    : true,
+              type        : 'POST',
+              url         : 'https://%@.infusionsoft.com/api/xmlrpc'.fmt(app.settings.subdomain)
+            };
+          },
+          _createTemplate = function(templateName, data) {
+            return app.renderTemplate(templateName, _.extend(serviceData, data));
+          },
+          _mapQueryFields = function(query) {
+            if (_.isObject(query)) {
+              var keys   = _.keys(query),
+                  values = _.values(query);
+
+              return _.map(keys, function(key, index) {
+                var value = values[index];
+                return {
+                  name  : key,
+                  type  : typeof(value),
+                  value : value
+                };
+              });
+            }
+          },
+          _parseResponse = function(xml, data) {
+            var validate = _validateResponse(xml),
+                $structs;
+
+            if (validate.valid && validate.$xml && ($structs = validate.$xml.find('struct'))) {
+              return $structs.get().map(function(struct, index) {
+                var member = {
+                  index: index
+                };
+                _.each(data.fields, function(field) {
+                  member[field] = app.$(struct).find('name:contains("' + field + '")').next().text();
+                });
+                return member;
+              });
+            }
+            return validate.message;
+          },
+          _sendRequest = function(templateName, data) {
+            var template = _createTemplate(templateName, data),
+                request  = _createRequest(template),
+                self     = this;
+
+            return app.promise(function(done, fail) {
+              app.ajax('get', request).then(
+                function(response) {
+                  var response = _parseResponse(response, data);
+                  if (_.isArray(response)) {
+                    done(response);
+                  } else {
+                    fail(response);
+                  }
+                },
+                function(message) {
+                  fail();
+                }
+              );
+            }).fail(function(message) {
+              app.gotoMessage(message);
+            });
+          },
+          _validateResponse = function(xml) {
+            var message   = 'DEFAULT MESSAGE',
+                valid     = false,
+                $fault,
+                $xml;
+
+            if (xml && ($xml = app.$(xml))) {
+              $fault  = $xml.find('fault');
+              if ($fault.length > 0) { message = $fault.find('name:contains("faultString")').next().text(); }
+              else { valid = true; }
+            }
+
+            return {
+              $xml    : $xml,
+              message : message,
+              valid   : valid
+            };
           };
 
       return {
         load: function(table, fields, id) {
           return app.promise(function(done, fail) {
             if (id > 0) {
-              this._sendRequest('dataService.load', {
+              _sendRequest('dataService.load', {
                 fields  : fields,
                 table   : table,
                 id      : id
@@ -51,80 +139,13 @@
           }.bind(this));
         },
         query: function(table, fields, query, limit, page) {
-          return this._sendRequest('dataService.query', {
+          return _sendRequest('dataService.query', {
             limit   : limit   || 1000,
             page    : page    || 0,
-            query   : query   || [],
+            query   : _mapQueryFields(query),
             fields  : fields,
             table   : table
           });
-        },
-
-        // private
-        _createRequest: function(xml) {
-          return {
-            contentType : 'application/xml',
-            data        : xml,
-            dataType    : 'xml',
-            proxy_v2    : true,
-            type        : 'POST',
-            url         : 'https://%@.infusionsoft.com/api/xmlrpc'.fmt(app.settings.subdomain)
-          };
-        },
-        _createTemplate: function(templateName, data) {
-          return app.renderTemplate(templateName, _.extend(serviceData, data));
-        },
-        _parseResponse: function(xml, data) {
-          var validate  = this._validateResponse(xml),
-              response  = [],
-              $structs;
-
-          if (validate.valid && validate.$xml && ($structs = validate.$xml.find('struct'))) {
-            return $structs.get().map(function(struct) {
-              var member = {};
-              _.each(data.fields, function(field) {
-                member[field] = app.$(struct).find('name:contains("' + field + '")').next().text();
-              });
-              return member;
-            });
-          }
-          return validate.message;
-        },
-        _sendRequest: function(templateName, data) {
-          var template = this._createTemplate(templateName, data),
-              request  = this._createRequest(template),
-              self     = this;
-
-          return app.promise(function(done, fail) {
-            app.ajax('get', request).then(function(response) {
-              var response = self._parseResponse(response, data);
-              if (_.isArray(response)) {
-                done(response);
-              } else {
-                fail(response);
-              }
-            });
-          }).fail(function(message) {
-            app.gotoMessage(message);
-          });
-        },
-        _validateResponse: function(xml) {
-          var message   = 'DEFAULT MESSAGE',
-              valid     = false,
-              $fault,
-              $xml;
-
-          if (xml && ($xml = app.$(xml))) {
-            $fault  = $xml.find('fault');
-            if ($fault.length > 0) { message = $fault.find('name:contains("faultString")').next().text(); }
-            else { valid = true; }
-          }
-
-          return {
-            $xml    : $xml,
-            message : message,
-            valid   : valid
-          };
         }
       };
     },
@@ -139,6 +160,7 @@
     getContactGroupCategories: function(groups) {
       return this.dataService.query('ContactGroupCategory', [ 'Id', 'CategoryName' ])
         .done(function(categories) {
+
           // Assign categories to group, TODO: Would be nicer without using global variable
           this.data.groups = groups.map(function(group) {
             return _.extend(group, {
@@ -147,12 +169,14 @@
           });
 
           // Initial load of contacts by email
-          this.getContacts('kiran@zendesk.com'); //TODO: this.ticket().requester().email());
+          this.getContacts(this.ticket().requester().email());
         }.bind(this));
     },
 
-    getContactOwner: function(id) {
-      return this.dataService.load('User', [ 'FirstName', 'LastName' ], id);
+    getContactOwner: function(contact) {
+      return this.dataService.load('User', [ 'FirstName', 'LastName' ], contact.OwnerID).done(function(owner) {
+        contact.Owner = owner;
+      });
     },
 
     getContacts: function(query) {
@@ -166,66 +190,41 @@
       // Create request
       var request, fields = [ 'FirstName', 'LastName', 'StreetAddress1', 'StreetAddress2', 'City', 'State', 'PostalCode', 'Country', 'Company', 'DateCreated', 'Email', 'Groups', 'Id', 'JobTitle', 'Leadsource', 'OwnerID', 'Phone1' ];
       if (this.isEmail(query)) {
-        request = this.dataService.query('Contact', fields);
+        request = this.dataService.query('Contact', fields, {
+          Email: query
+        });
       } else {
-        // TODO
+        request = this.dataService.query('Contact', fields, {
+          FirstName : query,
+          LastName  : query
+        });
       }
 
-      request.then(
-        function(contacts) {
-
-          // Need to work out the owner info
-          contacts = contacts.map(function(contact) {
-            this.getContactOwner(contact.OwnerID).done(function(owner) {
-              console.log('HERE', owner);
-            });
-
-            return _.extend(contact, {
-              Owner: {
-                FirstName : 'Travers',
-                LastName  : 'McInerney'
-              }
-            });
-          }.bind(this));
-
-          this.gotoContacts(contacts);
-        }.bind(this),
-
-        function(message) {
-          console.log(message);
-        }
-      );
-    },
-
-    getElementsForSection: function(e) {
-      var $heading = this.$(e.currentTarget),
-          $section = $heading.parent(),
-          $content = $section.find('.content'),
-          $contact = $section.parents('.contact');
-
-      return {
-        $contact  : $contact,
-        $content  : $content,
-        $heading  : $heading,
-        $section  : $section
-      };
+      request.then(function(contacts) {
+        this.data.contacts = contacts;
+        this.gotoContacts(contacts);
+      }.bind(this));
     },
 
     getGroupsForContact: function(e) {
-      var elements  = this.getElementsForSection(e),
-          contactId = elements.$contact.data('contact');
+      var $heading  = this.$(e.currentTarget),
+          $section  = $heading.parent(),
+          $content  = $section.find('.content'),
+          $contact  = $section.parents('.contact'),
+          contactId = $contact.data('contact');
 
       // Exit if we have already attempted to load data
-      if (elements.$section.data('loaded')) {
+      if ($section.data('loaded')) {
         return;
       }
 
       // Get groups for each contact
-      this.dataService.query('ContactGroupAssign', [ 'GroupId', 'ContactGroup', 'DateCreated' ], [{ name: 'ContactId', value: contactId }])
-        .done(function(response, state) {
-          var contactGroups = this.dataService.parseResponse(response, state).map(function(contactGroup) {
+      this.dataService.query('ContactGroupAssign', [ 'ContactId', 'GroupId', 'ContactGroup', 'DateCreated' ], {
+        ContactId: contactId
+      }).done(function(groups) {
+          groups = groups.map(function(contactGroup) {
             return {
-              category  : _.filter(this.data.groups, function(group) { console.log(group.Id, contactGroup); return group.Id === contactGroup.GroupId; }),
+              category  : _.filter(this.data.groups, function(group) { return group.Id === contactGroup.GroupId; }),
               created   : contactGroup.DateCreated,
               id        : contactGroup.GroupId,
               name      : contactGroup.ContactGroup
@@ -234,38 +233,40 @@
 
           // Generate template
           var $groups = this.renderTemplate('groups', {
-            groups: contactGroups
+            groups: groups
           });
 
           // Append content
-          elements.$content.html($groups);
+          $content.html($groups);
 
           // Set state to loaded
-          elements.$section.attr('loaded', true);
-        }.bind(this))
-        .fail(function() {
-          this.gotoMessage('Could not load data...');
-        });
+          $section.attr('loaded', true);
+        }.bind(this));
     },
 
     getOrdersAndSubscriptionsForContact: function(e) {
       var elements = this.getElementsForSection(e);
-      console.log(111);
     },
 
     gotoContacts: function(contacts) {
-      var templateData = {
-        contacts  : contacts,
-        matches   : contacts.length,
-        subdomain : this.settings.subdomain
-      };
+      var matches = contacts.length,
+          templateData = {
+            contacts  : contacts,
+            matches   : matches,
+            more      : matches > 5,
+            subdomain : this.settings.subdomain
+          };
 
       if (contacts.length > 0) {
-        this.switchTo('contacts', templateData);
-        return;
+        this.getContactOwner(_.first(contacts)).done(function(owner) {
+          this.switchTo('contacts', templateData);
+          var $contact = this.$('.contact:first-child');
+          $contact.find('.owner p').text('%@ %@'.fmt(owner.FirstName, owner.LastName));
+          $contact.addClass('active');
+        }.bind(this));
+      } else {
+        this.gotoMessage('We\'re sorry but it appears we were unable to match this end-user with an Infusionsoft contact. You can try searching instead', 'Contact not found');  
       }
-
-      this.gotoMessage('We\'re sorry but it appears we were unable to match this end-user with an Infusionsoft contact. You can try searching instead', 'Contact not found');
     },
 
     gotoIndex: function() {
@@ -273,7 +274,7 @@
     },
 
     gotoLoading: function() {
-      this.gotoMessage('Please wait. Loading data...','Loading');
+      this.gotoMessage('Please wait, loading data...','Loading');
     },
 
     gotoMessage: function(message, title) {
@@ -283,29 +284,33 @@
       });
     },
 
-    gotoSearch: function() {
-      this.switchTo('search', {
-        query: 'something'
-      });
-    },
-
     isEmail: function(value) {
-      return true;
+      return value.indexOf('@') !== -1;
     },
 
     onSearch: function() {
-      var query = this.$().find('.search-input').val();
-      this.getContacts(query);
+      this.getContacts(this.$().find('.search-input').val());
     },
 
     toggleContact: function(e) {
       var $toggle   = this.$(e.currentTarget),
           $icon     = $toggle.find('i'),
-          $contact  = $toggle.parents('.contact').toggleClass('inactive'),
-          isActive  = $contact.hasClass('inactive');
+          $contact  = $toggle.parents('.contact'),
+          $contacts = this.$().find('.contact').not($contact);
 
-      // TODO: This can be nicer
-      $icon.prop('class', '').addClass( isActive ? 'icon-chevron-down' : 'icon-chevron-up' );
+      // Get contact owner
+      if (!$contact.hasClass('active') && !$contact.data('owner')) {
+        var contactId = $contact.data('contact').toString(),
+            contact   = this.data.contacts.findProperty('Id', contactId);
+
+        this.getContactOwner(contact).done(function(owner) {
+          $contact.find('.owner p').text('%@ %@'.fmt(owner.FirstName, owner.LastName));
+          $contact.attr('data-owner', true);
+        });
+      }
+
+      $contacts.removeClass('active');
+      $contact.addClass('active');
     },
 
     toggleContactSection: function(e) {
@@ -316,6 +321,10 @@
 
       $section.addClass('active');
       $sections.removeClass('active');
+    },
+
+    toggleSearch: function(e) {
+      this.$('.search').toggleClass('active');
     }
   };
 
